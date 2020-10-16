89c56f8d9b7fd7818c8c48ef0f04bd16620056f1
AkremDev <akremdev@pop-os.localdomain>
2020-10-16 17:50:15 +0100

    make it better

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  };
+  return (
+    <div className="App">
+      <header>
+        <h1>Akrem To Do List</h1>
+      </header>
+      <Form
+        setStatus={setStatus}
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAngZ_bnv75ITvTTAuW_4g2LPJWSXG76N0",
+  authDomain: "to-do-list-akrem.firebaseapp.com",
+  databaseURL: "https://to-do-list-akrem.firebaseio.com",
+  projectId: "to-do-list-akrem",
+  storageBucket: "to-do-list-akrem.appspot.com",
+  messagingSenderId: "776525496542",
+  appId: "1:776525496542:web:1d8f5c2d6b67b12ef2824f",
+  measurementId: "G-KPQL29748T",
+};
